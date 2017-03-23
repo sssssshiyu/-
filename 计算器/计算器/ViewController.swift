ad7b7f248bc43fbdb524ec35b035f75c17aa7e8d
上海海洋大学 <shanhaikaiyoudaigaku@shanghaihaiyangdaxuedeimac.local>
2017-03-23 20:35:30 +0800

    初

--- 计算器/计算器/ViewController.swift
+++ 计算器/计算器/ViewController.swift
@@ -8,18 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func touchDigit(_ sender: UIButton) {
+
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
-}
 
