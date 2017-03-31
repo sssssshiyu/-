85c86c251667a8bfd2dd34c4e38974ad80f2f36c
上海海洋大学 <shanhaikaiyoudaigaku@shanghaihaiyangdaxuedeimac.local>
2017-03-31 18:09:48 +0800

    改

--- 计算器/计算器/ViewController.swift
+++ 计算器/计算器/ViewController.swift
@@ -10,10 +10,47 @@ import UIKit
 
 class ViewController: UIViewController{
 
+    
+    @IBOutlet weak var display: UILabel!
+    
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
-        print("\(digit) was touched")
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        }else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
     }
     
+    
+    var displayValue : Double{
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
     }
+    
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle{
+            switch mathematicalSymbol {
+            case "π":
+                displayValue = Double.pi
+            case "√":
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+    }
+}
 
